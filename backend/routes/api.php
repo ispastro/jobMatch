57171c0999e780635ab01f75f395cae8a908710d
haile <haileasaye51@gmail.com>
2025-04-30 16:01:28 +0300

    route api for job seeker

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobPostController;
+use App\Http\Controllers\JobSeekerController;
 
 Route::middleware('api')->group(function () {
     Route::apiResource('job-posts', JobPostController::class);
+    Route::apiResource('job-seekers', JobSeekerController::class);
 });
